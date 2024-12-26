cd65c1f329c9ec9e4e765112897c1c180cbc63ee
softscripts <softscripts@softscriptss-MacBook-Pro.local>
2024-12-26 18:15:57 +0530

    four

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = () => {
   return (
     <div>
-      <h1 className="flex items-center justify-center h-screen text-blue-100">Resume Page</h1>
+      <h1>Resume Page</h1>
       <p>This is the resume page content.</p>
     </div>
   );
