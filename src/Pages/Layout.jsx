bed61461251af4b35d4f7599302ef0cf6b5f6bc5
softscripts <softscripts@softscriptss-MacBook-Pro.local>
2024-12-27 13:16:11 +0530

    Seven

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <div>
-      <h1 className="Title">janelle Carothers</h1>
+      <h1 className="Title">janelle carothers</h1>
       <h2 className="sub">Costume Designer + Stylist</h2>
       <div>{children}</div>
     </div>
--- src/Pages/Portifoliyo.jsx
+++ src/Pages/Portifoliyo.jsx
@@ -1,11 +1,31 @@
-// src/components/Resume.js
 import React from 'react';
+import image1 from '../assests/Poti-1.png';
+import image2 from '../assests/Poti-02.png';
+import image3 from '../assests/Poti-3.jpg';
+import image4 from '../assests/Poti-4.jpg';
+import image5 from '../assests/Poti-5.jpg';
+import image6 from '../assests/Poti-6.jpg';
+import image7 from '../assests/poti-7.jpg';
+import image8 from '../assests/poti-8.jpg';
+import image9 from '../assests/poti-9.jpg';
+
+const imageList = [image1, image2, image3, image4, image5, image6, image7, image8, image9]; 
 
 const Portifoliyo = () => {
   return (
     <div>
-      <h1>Portifoliyo Page</h1>
-      <p>This is the resume page content.</p>
+      <div className="portfolio-grid">
+        {imageList.map((image, index) => (
+          <div className="portfolio-item" key={index}>
+            <div className="image-container">
+              <img src={image} alt={`Portfolio ${index + 1}`} className="image" />
+              <div className="image-overlay">
+                <div className="overlay-label">janelle Carothers<br></br><i style={{fontSize:"16px"}}>--view--</i></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
